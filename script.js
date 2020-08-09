25b368dba41aa3ddc4a60eb0155e92758b781c0b
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-08-09 13:49:21 -0700

    Add quotes to main application

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote From API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,7 +11,20 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        if (data.quoteAuthor === '') {
+            quthorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            // .classList looks at all of that element's CSS classes
+            // .classList.add(...) adds a class to the CSS for that element
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
     } catch (error) {
         getQuote(); // Try again and get a useable message
         console.log('No quote here! Error message: ', error)
